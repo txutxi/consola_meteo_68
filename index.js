020e7ddcb2348b172bc924a431dd263f15b46264
Txutxi <txutxi40@gmail.com>
2023-03-11 09:19:51 +0000

    Stage 67

---
+++ index.js
@@ -0,0 +1,24 @@
+const { leerInput } = require('./helpers/inquirer');
+
+require('colors') 
+
+
+
+
+
+
+
+
+
+const main = async() => {
+    const texto = await leerInput('Hola: ');
+    console.log(texto);
+};
+
+
+
+
+
+
+
+main();
